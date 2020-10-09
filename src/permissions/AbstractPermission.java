f8ba49c1a9a8fcca29100906b4a26e8c670a5272
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-08 22:08:12 -0400

    extract Action

--- src/permissions/AbstractPermission.java
+++ src/permissions/AbstractPermission.java
@@ -1,7 +1,5 @@
 package permissions;
 
-import users.User;
-
 import java.util.Objects;
 import java.util.Set;
 
@@ -16,29 +14,4 @@ public abstract class AbstractPermission implements Permission {
 
     public abstract Set<Action> allowed();
 
-    /**
-     * An {@code Action} specifies an attempt on the part of a {@code User} perform a change on the SHS system that requires {@code Permission}.
-     *
-     * @author Jeff Wilgus
-     * @see Permission
-     * @see User
-     */
-    public enum Action {
-
-        OPEN_DOOR {
-            @Override
-            public boolean isChildPermissible() {
-                return false;
-            }
-
-            @Override
-            public boolean isGuestPermissible() {
-                return false;
-            }
-        };
-
-        public abstract boolean isChildPermissible();
-        public abstract boolean isGuestPermissible();
-
-    }
 }
