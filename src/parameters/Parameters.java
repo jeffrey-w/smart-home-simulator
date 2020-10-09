86280be72e6f6a06fbc59ce39e4e5e4548ee3fb3
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-08 22:09:51 -0400

    initial commit

---
+++ src/parameters/Parameters.java
@@ -0,0 +1,77 @@
+package parameters;
+
+import permissions.Permission;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class Parameters {
+
+    private static final int DEFAULT_TEMPERATURE = 15;
+
+    private Permission permission;
+    private String location;
+    private LocalDate date;
+    private LocalTime time;
+    private int temperature;
+    private boolean on;
+
+    public Parameters() {
+        permission = null;
+        location = null;
+        date = LocalDate.now();
+        time = LocalTime.now();
+        temperature = DEFAULT_TEMPERATURE;
+        on = false;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = Objects.requireNonNull(date);
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = Objects.requireNonNull(time);
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+}
---
+++ src/permissions/Action.java
@@ -0,0 +1,90 @@
+package permissions;
+
+import users.User;
+
+/**
+ * An {@code Action} describes an attempt on the part of a {@code User} to change the SHS system, which requires {@code
+ * Permission}.
+ *
+ * @author Jeff Wilgus
+ * @see Permission
+ * @see User
+ */
+public enum Action {
+
+    CHANGE_TEMPERATURE {
+        @Override
+        public boolean isChildPermissible() {
+            return false;
+        }
+
+        @Override
+        public boolean isGuestPermissible() {
+            return false;
+        }
+
+    },
+
+    LOCK_DOOR {
+        @Override
+        public boolean isChildPermissible() {
+            return true;
+        }
+
+        @Override
+        public boolean isGuestPermissible() {
+            return false;
+        }
+
+    },
+
+    OPEN_DOOR {
+        @Override
+        public boolean isChildPermissible() {
+            return false;
+        }
+
+        @Override
+        public boolean isGuestPermissible() {
+            return true;
+        }
+
+    },
+
+    OPEN_WINDOW {
+        @Override
+        public boolean isChildPermissible() {
+            return false;
+        }
+
+        @Override
+        public boolean isGuestPermissible() {
+            return true;
+        }
+
+    },
+
+    TURN_ON_LIGHT {
+        @Override
+        public boolean isChildPermissible() {
+            return true;
+        }
+
+        @Override
+        public boolean isGuestPermissible() {
+            return true;
+        }
+
+    };
+
+    /**
+     * @return {@code true} if this {@code Action} is allowed to be taken by a child.
+     */
+    public abstract boolean isChildPermissible();
+
+    /**
+     * @return {@code true} if this {@code Action} is alloed to be taken by a guest.
+     */
+    public abstract boolean isGuestPermissible();
+
+}
