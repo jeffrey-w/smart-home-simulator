b5b96a758ee656ceecef6101d3e29e07e7099029
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-08 22:08:24 -0400

    formatting

--- src/permissions/ChildPermission.java
+++ src/permissions/ChildPermission.java
@@ -7,7 +7,8 @@ import java.util.stream.Collectors;
 
 public class ChildPermission extends AbstractPermission {
 
-    private static final Set<Action> ALLOWED = EnumSet.copyOf(Arrays.stream(Action.values()).filter(Action::isChildPermissible).collect(Collectors.toSet()));
+    private static final Set<Action> ALLOWED = EnumSet.copyOf(
+            Arrays.stream(Action.values()).filter(Action::isChildPermissible).collect(Collectors.toSet()));
 
     @Override
     public Set<Action> allowed() {
