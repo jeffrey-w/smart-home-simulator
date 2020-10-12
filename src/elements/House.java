329dc0814f74860fafc299a4f51a2f0265e7faf3
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-12 17:44:22 -0400

    implement as graph

--- src/elements/House.java
+++ src/elements/House.java
@@ -1,30 +1,86 @@
 // imports
-package src.elements;
+package elements;
 
-class House {
+import java.util.*;
+
+import static util.NameValidator.validateName;
+
+public class House implements Iterable<Room> {
+
+    private static class Node {
+
+        Room room;
+        Set<String> adjacents;
+
+        Node(Room room) {
+            this.room = Objects.requireNonNull(room);
+            this.adjacents = new HashSet<>();
+        }
+
+    }
+
     // variables
-    private String location = "default";
-    private Room[] rooms = {}; // FIXME
-    private HouseLayout houseLayout = null; // FIXME
+    private Map<String, Node> rooms;
 
     // constructor
-    House(String location, Room[] rooms, HouseLayout houseLayout) {
-        this.location = location;
-        this.rooms = rooms;
-        this.houseLayout = houseLayout;
+    public House() {
+        rooms = new HashMap<>();
     }
 
-    // methods
-    // get/set
-    public String getLocation() {
-        return location;
+    public void addRoom(Room room, String location) {
+        rooms.putIfAbsent(validateName(location), new Node(room)); // TODO throw exception if already present?
     }
 
-    public Room[] getRooms() {
-        return rooms;
+    public void addConnection(String locationOne, String locationTwo) {
+        Node roomOne = validateLocation(locationOne);
+        Node roomTwo = validateLocation(locationTwo);
+        roomOne.adjacents.add(locationTwo);
+        roomTwo.adjacents.add(locationOne);
     }
 
-    public HouseLayout getHouseLayout() {
-        return houseLayout;
+    private Node validateLocation(String location) {
+        if (rooms.containsKey(location)) {
+            return rooms.get(location);
+        }
+        throw new NoSuchElementException("That location does not exist");
     }
+
+    public Room removeRoom(String location) {
+        try {
+            return rooms.remove(location).room;
+        } catch (NullPointerException e) {
+            throw new NoSuchElementException("That location does not exist.");
+        }
+    }
+
+    @Override
+    public Iterator<Room> iterator() {
+        return new RoomIter();
+    }
+
+    private class RoomIter implements Iterator<Room> {
+
+        Iterator<Node> iterator;
+
+        RoomIter() {
+            iterator = rooms.values().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Room next() {
+            return iterator.next().room;
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();;
+        }
+
+    }
+
 }
