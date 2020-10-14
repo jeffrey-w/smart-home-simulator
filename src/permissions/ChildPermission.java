cecf37dd848180403cb8516e87764bc288bba5d7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-14 11:54:15 -0400

    add comments

--- src/permissions/ChildPermission.java
+++ src/permissions/ChildPermission.java
@@ -5,8 +5,15 @@ import java.util.EnumSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * The {@code ChildPermission} class specifies the {@code Action}s that a child actor is allowed to take in a
+ * simulation.
+ *
+ * @author Jeff Wilgus
+ */
 public class ChildPermission extends AbstractPermission {
 
+    // Filter the array containing all Actions on the Action.isChildPermissible predicate.
     private static final Set<Action> ALLOWED = EnumSet.copyOf(
             Arrays.stream(Action.values()).filter(Action::isChildPermissible).collect(Collectors.toSet()));
 
