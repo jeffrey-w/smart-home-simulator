37397284e88982c0ba1febc74a80247c73637aa7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-14 11:54:48 -0400

    add comments; add PERMISSIONS

--- src/permissions/AbstractPermission.java
+++ src/permissions/AbstractPermission.java
@@ -3,7 +3,22 @@ package permissions;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * The {@code AbstractPermission} class provides a minimal implementation of the {@code Permission} interface.
+ *
+ * @author Jeff Wilgus
+ */
 public abstract class AbstractPermission implements Permission {
+
+    /**
+     * A collection of default {@code Permission} levels.
+     */
+    public static final Permission[] PERMISSIONS = new Permission[] { // TODO rethink this
+            new ParentPermission(),
+            new ChildPermission(),
+            new GuestPermission(),
+            new StrangerPermission()
+    };
 
     @Override
     public void authorize(Action action) {
@@ -12,6 +27,10 @@ public abstract class AbstractPermission implements Permission {
         }
     }
 
+    /**
+     *
+     * @return the set of {@code Action}s that this {@code Permission} level is allowed to perform
+     */
     public abstract Set<Action> allowed();
 
     @Override
