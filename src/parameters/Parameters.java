8de0c890a2ecb9cc10579445920ba4d8825053bb
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-13 21:14:21 -0400

    make DEFAULT_TEMPERATURE public, add actors and getters and setters for it, refactor date and time into date

--- src/parameters/Parameters.java
+++ src/parameters/Parameters.java
@@ -2,44 +2,47 @@ package parameters;
 
 import permissions.Permission;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Objects;
+import java.time.Instant;
+import java.util.*;
 
 public class Parameters {
 
-    private static final int DEFAULT_TEMPERATURE = 15;
+    public static final int DEFAULT_TEMPERATURE = 15;
 
     private Permission permission;
+    private Map<String, Permission> actors;
     private String location;
-    private LocalDate date;
-    private LocalTime time;
+    private Date date;
     private int temperature;
     private boolean on;
 
     public Parameters() {
         permission = null;
+        actors = new HashMap<>();
         location = null;
-        date = LocalDate.now();
-        time = LocalTime.now();
+        date = Date.from(Instant.now());
         temperature = DEFAULT_TEMPERATURE;
         on = false;
+    }
+
+    public void addActor(String role, Permission permission) {
+        actors.putIfAbsent(role, permission);
     }
 
     public Permission getPermission() {
         return permission;
     }
 
+    public Map<String, Permission> getActors() {
+        return Collections.unmodifiableMap(actors);
+    }
+
     public String getLocation() {
         return location;
     }
 
-    public LocalDate getDate() {
+    public Date getDate() {
         return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
     }
 
     public int getTemperature() {
@@ -58,12 +61,8 @@ public class Parameters {
         this.location = location;
     }
 
-    public void setDate(LocalDate date) {
+    public void setDate(Date date) {
         this.date = Objects.requireNonNull(date);
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = Objects.requireNonNull(time);
     }
 
     public void setTemperature(int temperature) {
