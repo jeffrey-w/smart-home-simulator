dc14204ad8e76715e5341ca4314b4f1ac865b565
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-14 11:59:00 -0400

    extend JFrame instead of JPanel; modify constructor; add getLocationInput and addOkListener

--- src/view/ProfileEditor.java
+++ src/view/ProfileEditor.java
@@ -3,22 +3,37 @@ package view;
 import permissions.Permission;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
-public class ProfileEditor extends JPanel {
+public class ProfileEditor extends JFrame {
 
     JTextField role = new JTextField();
-    JComboBox<Permission> permission = new JComboBox<>();
+    JComboBox<Permission> permission = ParameterEditor.permissionJComboBox();
+    JComboBox<String> location = new JComboBox<>();
+    JButton ok = new JButton("Ok");
 
-    public ProfileEditor() {
-        setLayout(new SpringLayout());
-        add(new JLabel("Role"));
-        add(role);
-        add(new JLabel("Permission"));
-        add(permission);
-        for (Permission permission : ParameterEditor.PERMISSIONS) {
-            this.permission.addItem(permission);
+    public ProfileEditor(String role, boolean enableLocation) {
+        super("Edit Profile");
+        JPanel fields = new JPanel(new SpringLayout());
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setPreferredSize(new Dimension(256, 128));
+        setResizable(false);
+        fields.add(ParameterEditor.labelFactory("Role"));
+        fields.add(this.role);
+        fields.add(ParameterEditor.labelFactory("Permission"));
+        fields.add(permission);
+        fields.add(ParameterEditor.labelFactory("Location"));
+        fields.add(location);
+        SpringUtilities.makeCompactGrid(fields, 3, 2, 1, 1, 1, 16);
+        add(fields);
+        add(ok, BorderLayout.SOUTH);
+        if (role != null) {
+            this.role.setText(role);
+            this.role.setEnabled(false);
         }
-        SpringUtilities.makeCompactGrid(this, 2, 2, 1, 1, 1, 1);
+        location.setEnabled(enableLocation);
     }
 
     public String getRole() {
@@ -29,8 +44,12 @@ public class ProfileEditor extends JPanel {
         return (Permission)permission.getSelectedItem();
     }
 
-    public void setRole(final String role) {
-        this.role.setText(role);
-        this.role.setEnabled(false);
+    public String getLocationInput() { // TODO rename this
+        return (String)location.getSelectedItem();
     }
+
+    public void addOkListener(ActionListener listener) {
+        ok.addActionListener(listener);
+    }
+
 }
