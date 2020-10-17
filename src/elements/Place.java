a7926e844c74099f9d76454f7ee5f951bc552aa3
Ayman <aymalsh@gmail.com>
2020-10-18 01:47:52 +0300

    Make Room extend Place and move common implementation between Room and Yard to Place

--- src/elements/Place.java
+++ src/elements/Place.java
@@ -2,24 +2,36 @@ package elements;
 
 import permissions.Permission;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static util.NameValidator.validateName;
 
 public abstract class Place {
-    private ArrayList<Permission> occupants = new ArrayList<>();
+    private Map<String, Permission> people;
 
-    public ArrayList<Permission> getOccupants() {
-        return occupants;
+    public Place() {
+        people = new HashMap<>();
     }
 
-    public void setOccupants(ArrayList<Permission> occupants) {
-        this.occupants = occupants;
+    /**
+     * Add a person in a place
+     *
+     * @param name The name of the person/user to be added to the room
+     * @param permission The permission to add this person to the room
+     */
+    public void addPerson(final String name, final Permission permission) {
+        people.put(validateName(name), Objects.requireNonNull(permission));
     }
 
-    public void addOccupant(Permission occupant){
-        occupants.add(occupant);
-    }
-
-    public void removeOccupant(Permission occupant){
-        occupants.remove(occupant);
+    /**
+     * Remove a person from a place
+     *
+     * @param name The name of the person/user to remove from the list
+     */
+    public void removePerson(final String name) {
+        // TODO: Do we need to consider people with the same name located in the same room?
+        people.remove(name);
     }
 }
