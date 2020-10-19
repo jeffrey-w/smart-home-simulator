8ee0be8c35802d51782d3dfeaeb07c5135ed4dea
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-19 13:05:44 -0400

    formatting; add setObstructed

--- src/elements/Room.java
+++ src/elements/Room.java
@@ -1,34 +1,31 @@
 package elements;
 
 /**
- * A Room is comprised of many house elements, such as {@code Door}, {@code Light}, {@code Window}.
+ * A {@code Room} is comprised of many house elements, such as {@code Door}s, {@code Light}s, {@code Window}s.
  *
  * @author Philippe Vo
  * @author Émilie Martin
+ * @see Door
+ * @see Light
+ * @see Window
  */
-public class Room extends Place{
-    private String name;
+public class Room extends Place {
+
+    private static final int DEFAULT_ROOM_TEMPERATURE = 25;
+
     private int temperature;
-    private Door[] doors;
-    private Light[] lights;
-    private Window[] windows;
-
-    private final int DEFAULT_ROOM_TEMPERATURE = 25;
-
-    public void setObstructed(final int selection, boolean obstructed) {
-        windows[selection].setObstructed(obstructed);
-    }
+    private final Door[] doors;
+    private final Light[] lights;
+    private final Window[] windows;
 
     /**
-     * Constructs a Room with the given name, doors, lights, and windows
+     * Constructs a Room with the given doors, lights, and windows.
      *
-     * @param roomName The name of the room
      * @param doors An array of all doors contained within the room (location + lock state)
      * @param lights An array of all lights contained within the room (presence + on state)
      * @param windows An array of all windows contained within the room (location + obstruction state)
      */
-    public Room(String roomName, Door[] doors, Light[] lights, Window[] windows) {
-        this.name = roomName;
+    public Room(Door[] doors, Light[] lights, Window[] windows) {
         this.temperature = DEFAULT_ROOM_TEMPERATURE;
         this.doors = doors;
         this.lights = lights;
@@ -36,23 +33,8 @@ public class Room extends Place{
     }
 
     /**
-     * Setter to change the temperature of the room
-     * @param temp The new temperature
-     */
-    public void setTemperature(int temp) {
-        this.temperature = temp;
-    }
-
-    /**
-     * Getter that returns the name of the room
-     * @return The room name
-     */
-    public String getLocation() {
-        return this.name;
-    }
-
-    /**
      * Getter that returns the doors of a room
+     *
      * @return All doors contained within a room
      */
     public Door[] getDoors() {
@@ -61,6 +43,7 @@ public class Room extends Place{
 
     /**
      * Getter that returns the lights in a room
+     *
      * @return All lights contained within a room
      */
     public Light[] getLights() {
@@ -68,10 +51,36 @@ public class Room extends Place{
     }
 
     /**
-     * Getting that returns the windows of a room
+     * Getter that returns the windows of a room
+     *
      * @return All windows contained within a room
      */
     public Window[] getWindows() {
         return this.windows;
     }
+
+    /**
+     * Setter to change the temperature of the room
+     *
+     * @param temp The new temperature
+     */
+    public void setTemperature(int temp) {
+        this.temperature = temp;
+    }
+
+    /**
+     * Sets the {@code obstructed} status of the {@code Window} at the specified {@code wall} to that specified.
+     *
+     * @param wall the specified wall
+     * @param obstructed the specified obstructed status
+     * @throws IllegalArgumentException if the specified {@code wall} does not have a {@code Window}
+     */
+    public void setObstructed(int wall, boolean obstructed) {
+        try {
+            windows[wall].setObstructed(obstructed);
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("There is no window on that wall");
+        }
+    }
+
 }
