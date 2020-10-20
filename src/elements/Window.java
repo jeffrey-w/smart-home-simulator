c62afb4d9473fdfd1f26d18b4a8077e6e385acd7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-19 20:08:26 -0400

    remove references to ProximitySensor

--- src/elements/Window.java
+++ src/elements/Window.java
@@ -19,39 +19,30 @@ public class Window {
     private boolean located;
     private boolean isObstructed;
     private Bearing wall;
-    private ProximitySensor proximitySensor = null;
 
     /**
      * Constructs a Window with the given location, obstructed state, and wall.
      *
-     * @param loc This boolean refers to whether a window is located on a wall or not. Each room contains a Window array
-     * of length 4 to signify each wall (N-E-S-W)
-     * @param obstr This boolean refers to the state of the window, be it obstructed or not. If the boolean is true,
-     * then it is obstructed. If it is false, it is not obstructed.
+     * @param loc This boolean refers to whether a window is located on a wall or not. Each room contains a
+     *         Window array of length 4 to signify each wall (N-E-S-W)
+     * @param obstr This boolean refers to the state of the window, be it obstructed or not. If the boolean is
+     *         true, then it is obstructed. If it is false, it is not obstructed.
      * @param wall the wall that this window is on
      */
     public Window(boolean loc, boolean obstr, Bearing wall) {
         this.located = loc;
         this.isObstructed = obstr;
         this.wall = wall;
-        this.proximitySensor = new ProximitySensor(); // FIXME : Not sure if we should just create a sensor here out of "thin air"
     }
-    // FIXME : Not sure if we should pass "obstr" into the contructor of the Window Class.
-    // FIXME : [linked with] -> Not sure if we should have "obstructed" data in the "house JSON" file, it is only used to draw the house.
 
     /**
      * Outputs the state of the window to the user.
      */
-    void isObstructedString() {
-        if (this.isObstructed) {
-            System.out.println("This window is obstructed.");
-        } else {
-            System.out.println("This window is not obstructed.");
-        }
+    public boolean isObstructedString() {
+        return isObstructed;
     }
 
     /**
-     *
      * @return the wall that this {@code Window} is on
      */
     public Bearing getWall() {
@@ -67,26 +58,10 @@ public class Window {
     public void setObstructed(boolean obstr) {
         this.isObstructed = obstr;
     }
-    // FIXME : Not sure if we want to set obstructed here, should come from a sensor that detects if there are any obstacles.
 
     @Override
     public String toString() {
         return wall.toString();
     }
 
-    /**
-     * Outputs the state of the window to the user.
-     */
-    public boolean isBlocked() {
-        boolean blockState = this.proximitySensor.detect();
-
-        if(blockState) {
-            System.out.println("This window is obstructed.");
-        }
-        else {
-            System.out.println("This window is not obstructed.");
-        }
-        return blockState;
-    }
-    // FIXME : Modified how we detect the window block status by using a sensor since  this would make more sense with the theme of a "Smart Home"
 }
