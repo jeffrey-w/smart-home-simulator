9ce0eda006be204261660c0aff20c90def273e00
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-20 13:20:39 -0400

    edit comments

--- src/test/elements/WindowTest.java
+++ src/test/elements/WindowTest.java
@@ -9,15 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WindowTest {
 
-    // testing window.setObstructed + window.isObstructedString (blocking window use case)
+    // Testing window.setObstructed + window.isObstructedString (blocking window use case)
     @Test
     void testIsBlocked() {
         Window window = new Window(true, true, Wall.EAST);
 
-        // window should return that it is blocked since it was initialized as obstructed
+        // Window should return that it is blocked since it was initialized as obstructed
         assertTrue(window.isObstructed());
 
-        // window should return that it is not blocked
+        // Window should return that it is not blocked
         window.setObstructed(false);
         assertFalse(window.isObstructed());
     }
