94262b9fb260473f20bc98b35e08820823aceb73
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-26 22:45:47 -0400

    remove event handling logic; change type parameters; add getSelectedItem and getSelectedAction

--- src/main/view/ActionPanel.java
+++ src/main/view/ActionPanel.java
@@ -1,64 +1,62 @@
 package main.view;
 
+import main.model.parameters.permissions.Action;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseListener;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The {@code ActionPanel} class provides the UI elements to choose a type of item and the action to perform
- * on it during a simulation.
+ * The {@code ActionPanel} class provides the UI elements to choose a type of item and the action to perform on it
+ * during a simulation.
  *
  * @author Jeff Wilgus
  */
 public class ActionPanel extends JPanel {
 
-    static MouseListener WINDOW_ACTION_LISTENER; // TODO make this a member?
-    private static final Map<String, String[]> ACTIONS = new HashMap<>();
+    private static final Map<String, Action[]> ACTIONS = new HashMap<>();
 
     static {
-        ACTIONS.put("Windows", new String[] {"Open/Close", "Obstruct"});
-        ACTIONS.put("Thermostat", new String[] {"Set Temperature"});
+        ACTIONS.put("Windows", new Action[] {Action.OPEN_WINDOW, Action.BLOCK_WINDOW});
     }
 
-    DefaultListModel<String> actionsModel = new DefaultListModel<>();
+    DefaultListModel<Action> actionsModel = new DefaultListModel<>();
     JList<String> items = new JList<>(ACTIONS.keySet().toArray(new String[0]));
-    JList<String> actions = new JList<>(actionsModel);
+    JList<Action> actions = new JList<>(actionsModel);
 
     /**
      * Constructs a new {@code ActionPanel} object.
      */
     public ActionPanel() {
-        JPanel top = new JPanel(new BorderLayout());
-        JPanel bottom = new JPanel(new BorderLayout());
+        JPanel top = new JPanel(new BorderLayout()), bottom = new JPanel(new BorderLayout());
         setLayout(new BorderLayout());
-        add(top, BorderLayout.NORTH);
+        add(top);
         add(bottom, BorderLayout.SOUTH);
         top.add(new JLabel("Items"), BorderLayout.NORTH);
-        top.add(new JScrollPane(items));
+        top.add(items);
         bottom.add(new JLabel("Actions"), BorderLayout.NORTH);
-        bottom.add(new JScrollPane(actions));
-        items.setPreferredSize(new Dimension(576 - 32, 215)); // TODO avoid magic constants
-        actions.setPreferredSize(new Dimension(576 - 32, 215));
+        bottom.add(actions);
         items.addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
                 if (items.getSelectedIndex() == -1) {
                     actionsModel.removeAllElements();
                 } else {
                     actionsModel.removeAllElements();
-                    for (String action : ACTIONS.get(items.getSelectedValue())) {
+                    for (Action action : ACTIONS.get(items.getSelectedValue())) {
                         actionsModel.addElement(action);
-                    }
-                    switch (items.getSelectedValue()) {
-                        case "Windows":
-                            actions.addMouseListener(WINDOW_ACTION_LISTENER);
-                            break;
-                        // TODO other event handlers
                     }
                 }
             }
         });
+    }
+
+    public String getSelectedItem() {
+        return items.getSelectedValue();
+    }
+
+    public Action getSelectedAction() {
+        return actions.getSelectedValue();
     }
 
 }
