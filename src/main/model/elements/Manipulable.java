50aca053aea50c39c8cac6a3cb3161903dad92d7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-29 00:28:01 -0400

    add comments

--- src/main/model/elements/Manipulable.java
+++ src/main/model/elements/Manipulable.java
@@ -2,9 +2,20 @@ package main.model.elements;
 
 import main.model.parameters.permissions.Action;
 
-// TODO comment this
+/**
+ * The {@code Manipulable} interface describes objects whose physical configurations may be changed by performing {@code
+ * Action}s on them.
+ *
+ * @author Jeff Wilgus
+ * @see Action
+ */
 public interface Manipulable {
 
-    void doAction(Action action);
+    /**
+     * Performs the specified {@code action} on this {@code Manipulable}.
+     *
+     * @param action the specified {@code Action}
+     */
+    void manipulate(Action action);
 
 }
