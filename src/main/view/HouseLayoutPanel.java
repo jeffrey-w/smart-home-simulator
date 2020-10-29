3534c59fbd37a998afc5f0e0f44a995ab1765eae
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-10-29 00:34:32 -0400

    add drawHouse; modify paintComponent

--- src/main/view/HouseLayoutPanel.java
+++ src/main/view/HouseLayoutPanel.java
@@ -4,55 +4,30 @@ import main.model.elements.House;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * The {@code HouseLayoutPanel} class provides the UI elements for visualizing a {@code House}.
  *
  * @author Jeff Wilgus
  */
-public class HouseLayoutPanel extends JPanel { // TODO move this logic to Controller
+public class HouseLayoutPanel extends JPanel {
 
     private static final int ROOM_DIM = 100;
     private static final String NULL_HOUSE_MESSAGE = "No house loaded.";
 
     int x, y;
     int drawn = 0;
-    House house;
+    Map<String, Point> rooms = new LinkedHashMap<>();
 
-    /**
-     * Constructs a new {@code HouseLayoutPanel} object.
-     *
-     * @param house The {@code House} object being visualized
-     */
-    public HouseLayoutPanel(House house) {
-        setHouse(house);
-    }
-
-    /**
-     * Sets the {@code House} object being visualized to that specified. Calls to this method cause this panel to be
-     * redrawn.
-     *
-     * @param house the {@code House} being visualized
-     */
-    public void setHouse(House house) {
-        this.house = house;
-        repaint();
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (house == null) {
-            x = getWidth() - g.getFontMetrics().stringWidth(NULL_HOUSE_MESSAGE) >>> 1;
-            y = getHeight() >>> 1;
-            g.drawString("No house loaded.", x, y);
-        } else {
+    public void drawHouse(House house) {
+        rooms.clear();
+        if (house != null) {
             x = getWidth() >>> house.size() - 1;
             y = getHeight() >>> house.size() - 1;
             house.tour((location, room) -> {
-                g.drawRect(x, y, ROOM_DIM, ROOM_DIM);
-                g.drawString(location, x + (ROOM_DIM - g.getFontMetrics().stringWidth(location) >>> 1),
-                        y + (ROOM_DIM >>> 1));
+                rooms.put(location, new Point(x, y));
                 if ((++drawn & 1) == 0) { // drawn + 1 is even
                     x += ROOM_DIM;
                 } else {
@@ -60,6 +35,25 @@ public class HouseLayoutPanel extends JPanel { // TODO move this logic to Contro
                 }
             });
         }
+        revalidate();
+        repaint();
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (rooms.isEmpty()) {
+            x = getWidth() - g.getFontMetrics().stringWidth(NULL_HOUSE_MESSAGE) >>> 1;
+            y = getHeight() >>> 1;
+            g.drawString(NULL_HOUSE_MESSAGE, x, y);
+        } else {
+            for (Map.Entry<String, Point> entry : rooms.entrySet()) {
+                int x = entry.getValue().x, y = entry.getValue().y;
+                String location = entry.getKey();
+                g.drawRect(x, y, ROOM_DIM, ROOM_DIM);
+                g.drawString(location, x + (ROOM_DIM - g.getFontMetrics().stringWidth(location) >>> 1),
+                        y + (ROOM_DIM >>> 1));
+            }
+        }
+    }
 }
