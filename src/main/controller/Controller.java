af3f7016dd870c11f12d755e4b3f0ca8024b7a47
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-03 21:30:25 -0500

    modify ActionSelectionListener::mouseClicked

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -237,6 +237,7 @@ public class Controller {
                             dashboard.sendToConsole(
                                     actionPanel.getSelectedAction() + " performed on " + chooser.getSelectedItem()
                                             + " of " + parameters.getLocation() + ".");
+                            dashboard.blockWindowIn(parameters.getLocation());
                         } catch (IllegalArgumentException exception) {
                             dashboard.sendToConsole(exception.getMessage());
                         }
