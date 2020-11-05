621fda54a3b7057cae1c95c46f44797d24ff463f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-05 17:51:07 -0500

    initial commit

---
+++ src/main/model/Module.java
@@ -0,0 +1,60 @@
+package main.model;
+
+import main.model.parameters.permissions.Action;
+import main.util.ModuleBuilder;
+
+import java.util.*;
+
+import static main.util.NameValidator.validateName;
+
+public final class Module {
+
+    public static final Module SHC = (new ModuleBuilder())
+            .addCommandFor("Doors", Action.OPEN_DOOR)
+            .addCommandFor("Doors", Action.LOCK_DOOR)
+            .addCommandFor("Lights", Action.TURN_ON_LIGHT)
+            .addCommandFor("Windows", Action.OPEN_WINDOW)
+            .addCommandFor("Windows", Action.BLOCK_WINDOW)
+            .addName("SHC")
+            .build();
+
+    public static final Module SHP = (new ModuleBuilder())
+            //.addCommandFor("Away Mode", Action.SET_AWAY_MODE) TODO
+            //.addCommandFor("Lights", Action.SET_AWAY_MODE_LIGHTS)
+            .addName("SHP")
+            .build();
+
+    private final String name;
+    private final Map<String, Set<Action>> commands;
+
+
+    public Module(String name) {
+        this.name = validateName(name);
+        commands = new HashMap<>();
+    }
+
+    public void addCommand(String item, Action action) {
+        Set<Action> actions = commands.getOrDefault(item, new HashSet<>()); // TODO validate item
+        actions.add(Objects.requireNonNull(action));
+        commands.putIfAbsent(item, actions);
+    }
+
+    public void addCommands(String item, Collection<Action> actions) {
+        Set<Action> actionSet = commands.getOrDefault(item, new HashSet<>()); // TODO validate item
+        actionSet.addAll(actions);
+        commands.putIfAbsent(item, actionSet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<String> getItems() {
+        return Collections.unmodifiableSet(commands.keySet());
+    }
+
+    public Set<Action> getActionsFor(String item) {
+        return Collections.unmodifiableSet(commands.get(item)); // TODO validate item
+    }
+
+}
---
+++ src/main/view/ModuleView.java
@@ -0,0 +1,44 @@
+package main.view;
+
+import main.model.parameters.permissions.Action;
+import main.model.Module;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class ModuleView extends JPanel {
+
+    private static final int NUM_ROWS = 2;
+
+    final Module module;
+    final DefaultListModel<String> itemsModel = new DefaultListModel<>();
+    final DefaultListModel<Action> actionsModel = new DefaultListModel<>();
+    final JList<String> items = new JList<>(itemsModel);
+    final JList<Action> actions = new JList<>(actionsModel);
+
+    public ModuleView(Module module) {
+        JScrollPane itemsPane = new JScrollPane(items);
+        JScrollPane actionPane = new JScrollPane(actions);
+        this.module = Objects.requireNonNull(module);
+        setLayout(new GridLayout(NUM_ROWS, 1));
+        add(itemsPane);
+        add(actionPane);
+        for (String item : module.getItems()) {
+            itemsModel.addElement(item);
+        }
+        items.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                if (items.getSelectedIndex() == -1) {
+                    actionsModel.removeAllElements();
+                } else {
+                    actionsModel.removeAllElements();
+                    for (Action action : module.getActionsFor(items.getSelectedValue())) {
+                        actionsModel.addElement(action);
+                    }
+                }
+            }
+        });
+    }
+
+}
