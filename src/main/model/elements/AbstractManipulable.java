e559e7defa541c5c34639df04027e369960e8fa5
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-06 11:08:18 -0500

    add comments

--- src/main/model/elements/AbstractManipulable.java
+++ src/main/model/elements/AbstractManipulable.java
@@ -2,6 +2,12 @@ package main.model.elements;
 
 import main.model.parameters.permissions.Action;
 
+/**
+ * The {@code AbstractManipulable} class provides a minimal implementation of the {@code Manipulable} interface.
+ *
+ * @author Jeff Wilgus
+ * @author Phillipe Vo
+ */
 public abstract class AbstractManipulable implements Manipulable {
 
     @Override
