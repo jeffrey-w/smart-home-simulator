126f12d9c3020e835943d840c952c641e3d081f8
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-08 16:48:06 -0500

    add JavaDoc

--- src/main/model/ValueManipulable.java
+++ src/main/model/ValueManipulable.java
@@ -1,18 +1,36 @@
 package main.model;
 
 import main.model.elements.AbstractManipulable;
+import main.model.parameters.Parameters;
 
 import java.util.Objects;
 
+/**
+ * The {@code ValueManipulable} class represents an abstract value chosen for simulation {@code Parameters}.
+ *
+ * @param <T> The domain of this {@code ValueManipulable}
+ *
+ * @author Jeff Wilgus
+ * @see Parameters
+ */
 public class ValueManipulable<T> extends AbstractManipulable {
 
-    T value;
+    private final T value;
 
+    /**
+     * Constructs a new {@code ValueManipulable} with the specified {@code value}.
+     * @param value The value held by this {@code ValueManipulable}
+     * @throws NullPointerException If the specified {@code value} is {@code null}
+     */
     public ValueManipulable(T value) {
         this.value = Objects.requireNonNull(value);
     }
 
-    T getValue() {
+    /**
+     *
+     * @return The value held by this {@code ValueManipulable}
+     */
+    public T getValue() {
         return value;
     }
 
--- src/main/view/AwayLightChooser.java
+++ src/main/view/AwayLightChooser.java
@@ -1,5 +1,9 @@
 package main.view;
 
+import main.model.elements.Light;
+import main.model.elements.Room;
+import main.model.parameters.AwayMode;
+
 import javax.swing.*;
 import javax.swing.text.DateFormatter;
 import java.awt.*;
@@ -10,11 +14,27 @@ import java.util.*;
 
 import static java.awt.BorderLayout.SOUTH;
 
+/**
+ * The {@code AwayLightChooser} class provides the UI elements to select the specific {@code Room}s and {@code Time}s
+ * during which {@code Lights} shall be on during {@code AwayMode}
+ *
+ * @author Jeff Wilgus
+ * @see AwayMode
+ * @see Light
+ * @see Room
+ */
 public class AwayLightChooser extends JFrame {
 
     private static final int DIM = 0x100;
     private static final int CELLS = 2;
 
+    /**
+     * Provides a new {@code AwayLightChooser} of the specified {@code locations}.
+     *
+     * @param locations the specified locations
+     * @return A new {@code AwayLightChooser}
+     * @throws NullPointerException If the specified {@code locations} are {@code null}
+     */
     public static AwayLightChooser of(Set<String> locations) {
         AwayLightChooser awayLightChooser = new AwayLightChooser();
         awayLightChooser.addLocations(locations);
@@ -28,7 +48,9 @@ public class AwayLightChooser extends JFrame {
     private final JSpinner.DateEditor endEditor = new JSpinner.DateEditor(endSpinner, "HH:mm");
     private final JButton ok = new JButton("Ok");
 
-
+    /**
+     * @return The locations selected by this {@code AwayLightChooser}
+     */
     public Set<String> getSelectedLocations() {
         Set<String> selected = new HashSet<>();
         for (JCheckBox box : locations) {
@@ -39,18 +61,29 @@ public class AwayLightChooser extends JFrame {
         return selected;
     }
 
+    /**
+     * @return The selected start time for away light mode
+     */
     public LocalTime getStart() {
         Calendar calendar = new GregorianCalendar();
         calendar.setTime((Date) startSpinner.getModel().getValue());
         return LocalTime.of(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
     }
 
+    /**
+     * @return The selected end time for away light mode
+     */
     public LocalTime getEnd() {
         Calendar calendar = new GregorianCalendar();
         calendar.setTime((Date) endSpinner.getModel().getValue());
         return LocalTime.of(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
     }
 
+    /**
+     * Registers an event handler for on this {@code AwayLightChooser}
+     *
+     * @param listener The specified event handler
+     */
     public void addActionListener(ActionListener listener) {
         ok.addActionListener(listener);
     }
@@ -61,7 +94,7 @@ public class AwayLightChooser extends JFrame {
         JPanel staticFields = new JPanel(new GridLayout(CELLS, 1));
         JPanel timeSpinners = new JPanel(new GridLayout(1, CELLS));
         DateFormatter startFormatter = (DateFormatter) startEditor.getTextField().getFormatter();
-        DateFormatter endFormatter = (DateFormatter)endEditor.getTextField().getFormatter();
+        DateFormatter endFormatter = (DateFormatter) endEditor.getTextField().getFormatter();
         startFormatter.setAllowsInvalid(false);
         startFormatter.setOverwriteMode(true);
         endFormatter.setAllowsInvalid(false);
