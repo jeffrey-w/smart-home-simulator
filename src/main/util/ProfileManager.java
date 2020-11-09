93a1e4822be5be2eb7a3aae9b1eac111ff721873
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-08 19:48:29 -0500

    modify loadProfiles and saveProfiles

--- src/main/util/ProfileManager.java
+++ src/main/util/ProfileManager.java
@@ -10,27 +10,29 @@ import java.util.Scanner;
 
 public abstract class ProfileManager {
 
-    public static HashMap<String, Permission> loadProfiles(File file) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, FileNotFoundException {
+    public static HashMap<String, Permission> loadProfiles(File file)
+            throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+            InstantiationException, FileNotFoundException {
         HashMap<String, Permission> actors = new HashMap<>();
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] lineArray = line.split(" ");
-                String name = lineArray[0];
-                String permissionString = lineArray[1] + "Permission";
-                Class<?> klass = Class.forName("main.model.parameters.permissions." + permissionString);
-                Permission permission = (Permission) klass.getDeclaredConstructor().newInstance();
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] lineArray = line.split(", ");
+            String name = lineArray[0];
+            String permissionString = lineArray[1] + "Permission";
+            Class<?> klass = Class.forName("main.model.parameters.permissions." + permissionString);
+            Permission permission = (Permission) klass.getDeclaredConstructor().newInstance();
 
-                actors.put(name, permission);
-            }
-
+            actors.put(name, permission);
+        }
+        scanner.close();
         return actors;
     }
 
     public static void saveProfiles(Map<String, Permission> actors, File file) throws IOException {
         BufferedWriter out = new BufferedWriter(new FileWriter(file.getPath()));
         for (Map.Entry<String, Permission> item : actors.entrySet()) {
-            out.write(item.getKey() + " " + item.getValue());
+            out.write(item.getKey() + ", " + item.getValue());
             out.newLine();
         }
         out.close();
