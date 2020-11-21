98f6baaa4d9d3d49d9f58e3dceb7c41d06f799a7
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-20 19:21:56 -0500

    modify control; add canAct, toggleAutoLight, and getItemsForSelection

--- src/main/controller/CoreModuleController.java
+++ src/main/controller/CoreModuleController.java
@@ -1,31 +1,31 @@
 package main.controller;
 
 import main.model.Action;
-import main.model.Module;
+import main.model.Manipulable;
 import main.model.ValueManipulable;
+import main.model.elements.Room;
+import main.model.elements.Yard;
 import main.model.parameters.Parameters;
+import main.view.Dashboard;
 import main.view.ItemChooser;
 import main.view.ModuleView;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class CoreModuleController extends AbstractModuleController implements ActionListener {
+public class CoreModuleController extends AbstractModuleController {
 
     public CoreModuleController(Controller parent, ModuleView view) {
-        super(parent, Module.SHC, view);
+        super(parent, view);
     }
 
     @Override
-    public void actionPerformed(final ActionEvent e) {
-        if (parent.canAct()) {
+    public void control() {
+        if (canAct()) {
             Parameters parameters = parent.getParameters();
-            Action action = parent.getSelectedAction();
+            Action action = view.getSelectedAction();
             if (action.equals(Action.TOGGLE_AUTO_LIGHT)) {
                 performCommand(new ValueManipulable<>(!parameters.isAutoLight()), action);
-                parent.toggleAutoLight();
+                toggleAutoLight();
             } else {
-                ItemChooser chooser = ItemChooser.of(parent.getItemsForSelection());
+                ItemChooser chooser = ItemChooser.of(getItemsForSelection());
                 chooser.addActionListener(f -> {
                     performCommand(chooser.getSelectedItem(), action);
                     chooser.dispose();
@@ -35,6 +35,42 @@ public class CoreModuleController extends AbstractModuleController implements Ac
                 chooser.setVisible(true);
             }
             parent.redrawHouse();
+        } else {
+            String message = "Please select a permission and location to choose an action.";
+            if (parent.getHouse() == null) {
+                message += " You must first load a house to select a location.";
+            }
+            parent.sendToConsole(message, Dashboard.MessageType.ERROR);
+        }
+    }
+
+    @Override
+    public boolean canAct() {
+        return !(parent.getParameters().getPermission() == null || parent.getParameters().getLocation() == null);
+    }
+
+    public void toggleAutoLight() {
+        if (parent.getParameters().isAutoLight()) {
+            for (Room room : parent.getHouse()) {
+                room.toggleLights(room.isOccupied());
+            }
+            if (Yard.getInstance().isOccupied()) {
+                Yard.getInstance().setLightOn(true);
+            }
+        }
+    }
+
+    private Manipulable[] getItemsForSelection() {
+        Room room = parent.getHouse().getRoom(parent.getParameters().getLocation());
+        switch (view.getSelectedItem()) {
+            case "Doors":
+                return room.getDoors();
+            case "Lights":
+                return room.getLights();
+            case "Windows":
+                return room.getWindows();
+            default:
+                throw new AssertionError(); // Defensive measure; this should never happen.
         }
     }
 
