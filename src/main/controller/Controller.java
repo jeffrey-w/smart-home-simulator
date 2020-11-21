62f3c414855d691c45ba4f8a1efdc723fe13f9b1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-20 21:42:54 -0500

    refactor parameter editor listeners out

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,31 +1,17 @@
 package main.controller;
 
-import main.model.Action;
 import main.model.Module;
 import main.model.elements.House;
 import main.model.parameters.Parameters;
-import main.model.parameters.permissions.Permission;
-import main.util.HouseReader;
-import main.util.JSONFilter;
-import main.util.PermissionManager;
-import main.util.TextFilter;
 import main.view.Dashboard;
-import main.view.PermissionEditor;
-import main.view.ProfileEditor;
-import main.view.ProfileViewer;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 /**
  * The {@code Controller} class provides the interface between runtime simulation objects and the UI elements to
@@ -35,11 +21,8 @@ import java.util.NoSuchElementException;
  * @author Émilie Martin
  */
 public class Controller {
-    private static final JSONFilter JSON_FILTER = new JSONFilter();
-    private static final TextFilter TEXT_FILTER = new TextFilter();
 
     // This is the entry point into the program
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             try {
@@ -63,30 +46,40 @@ public class Controller {
      * Constructs a new {@code Controller} object.
      */
     public Controller() {
+        ParameterController parameterController = new ParameterController(this);
         addModules();
+        startClockDisplayUpdate();
         dashboard.setTemperature(String.valueOf(parameters.getTemperature()));
         dashboard.setDate(parameters.getDate());
         dashboard.addSimulationListener(new SimulationListener());
-        dashboard.addLoadHouseListener(new LoadHouseListener());
-        dashboard.addEditProfilesListener(new ManageProfilesListener());
-        dashboard.addManageProfilesListener(new EditProfileListener());
-        dashboard.addPersistPermissionListener(new PersistPermissionListener());
-        dashboard.addPermissionListener(new PermissionListener());
-        dashboard.addEditPermissionListener(new EditPermissionsListener());
-        dashboard.addTemperatureListener(new TemperatureListener());
-        dashboard.addDateListener(new DateListener());
-        dashboard.addTimeXListener(new TimeXListener());
-        dashboard.addTimeUpdateListener(new TimeUpdateListener());
+        dashboard.addLoadHouseListener(parameterController.new LoadHouseListener());
+        dashboard.addEditProfilesListener(parameterController.new ManageProfilesListener());
+        dashboard.addManageProfilesListener(parameterController.new EditProfileListener());
+        dashboard.addPersistPermissionListener(parameterController.new PersistPermissionListener());
+        dashboard.addPermissionListener(parameterController.new PermissionListener());
+        dashboard.addEditPermissionListener(parameterController.new EditPermissionsListener());
+        dashboard.addTemperatureListener(parameterController.new TemperatureListener());
+        dashboard.addDateListener(parameterController.new DateListener());
+        dashboard.addTimeXListener(parameterController.new TimeXListener());
+        dashboard.addTimeUpdateListener(parameterController.new TimeUpdateListener());
         dashboard.addActionSelectionListener(new ActionSelectionListener());
         dashboard.drawHouse(house);
-
-        // start clock update
-        startClockDisplayUpdate();
     }
 
     private void addModules() {
         modules.put(Module.SHC.getName(), new CoreModuleController(this, dashboard.addModule(Module.SHC)));
         modules.put(Module.SHP.getName(), new SecurityModuleController(this, dashboard.addModule(Module.SHP)));
+    }
+
+    private void startClockDisplayUpdate() {
+        ActionListener updateClockListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dashboard.setTime(parameters.getClockTime());
+            }
+        };
+
+        javax.swing.Timer timerClock = new javax.swing.Timer(1000, updateClockListener);
+        timerClock.start();
     }
 
     /*
@@ -116,253 +109,6 @@ public class Controller {
 
     }
 
-    class LoadHouseListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setFileFilter(JSON_FILTER);
-            if (chooser.showOpenDialog(dashboard) == JFileChooser.APPROVE_OPTION) {
-                HouseReader reader = new HouseReader(chooser.getSelectedFile());
-                house = reader.readHouse();
-                dashboard.addLocationListener(null);
-                dashboard.activateLocations(house.getLocations());
-                dashboard.addLocationListener(new LocationListener());
-                dashboard.drawHouse(house);
-            }
-        }
-
-    }
-
-    class ManageProfilesListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            ProfileViewer viewer = dashboard.getProfileViewer();
-            viewer.clear();
-            viewer.populateList(parameters.getActorsIdentifier());
-            viewer.pack();
-            viewer.setLocationRelativeTo(dashboard);
-            viewer.setVisible(true);
-        }
-
-    }
-
-    class EditProfileListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            String actionCommand = e.getActionCommand();
-            ProfileViewer viewer = dashboard.getProfileViewer();
-            switch (actionCommand) {
-                case "Add":
-                case "Edit": {
-                    ProfileEditor editor = new ProfileEditor(viewer.getSelectedValue(), house != null);
-                    editor.setPermission(parameters.permissionOf(editor.getRole()));
-                    if (house != null) {
-                        editor.addLocations(house.getLocations());
-                        if (editor.getRole() != null && house.contains(editor.getRole())) {
-                            editor.selectLocation(house.locationOf(editor.getRole()));
-                        }
-                    }
-                    editor.addActionListener(f -> {
-                        String name = editor.getRole();
-                        Permission permission = editor.getSelectedPermission();
-                        String location = editor.getSelectedLocation();
-                        try {
-                            parameters.addActor(name, permission);
-                            if (location != null) { // Assume that house is non-null since location field is enabled.
-                                house.addPerson(name, permission, location);
-                                sendToConsole(name + " has entered the " + location + ".",
-                                        Dashboard.MessageType.NORMAL);
-                                getCoreModuleController().toggleAutoLight();
-                                redrawHouse();
-                                if (parameters.isOn() && parameters.isAwayMode()) {
-                                    getSecurityModuleController().startAwayModeCountdown();
-                                }
-                            } else {
-                                if (house != null && house.removePerson(name)) {
-                                    sendToConsole(name + " has exited the house.", Dashboard.MessageType.NORMAL);
-                                }
-                            }
-                            if (!viewer.containsProfile(name)) {
-                                viewer.addProfile(name);
-                            }
-                            editor.dispose();
-                        } catch (Exception exception) {
-                            sendToConsole(exception.getMessage(), Dashboard.MessageType.ERROR);
-                        }
-                    });
-                    editor.pack();
-                    editor.setLocationRelativeTo(viewer);
-                    editor.setVisible(true);
-                    break;
-                }
-                case "Remove": {
-                    parameters.removeActor(viewer.getSelectedValue());
-                    if (house != null) {
-                        house.removePerson(viewer.getSelectedValue());
-                        sendToConsole(viewer.getSelectedValue() + " has exited the house.",
-                                Dashboard.MessageType.NORMAL);
-                        getCoreModuleController().toggleAutoLight();
-                        redrawHouse();
-                    }
-                    viewer.removeProfile(viewer.getSelectedValue());
-                    break;
-                }
-                default:
-                    throw new AssertionError(); // Defensive measure; this should never happen.
-            }
-        }
-
-    }
-
-    class PersistPermissionListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            String actionCommand = e.getActionCommand();
-            switch (actionCommand) {
-                case "Load Permissions": {
-                    JFileChooser chooser = new JFileChooser();
-                    chooser.setFileFilter(TEXT_FILTER);
-                    if (chooser.showOpenDialog(dashboard) == JFileChooser.APPROVE_OPTION) {
-                        try {
-                            parameters.setPermissions(PermissionManager.loadPermissions(chooser.getSelectedFile()));
-                            //TODO update profile list
-                        } catch (Exception exception) {
-                            sendToConsole(exception.getMessage(), Dashboard.MessageType.ERROR);
-                        }
-                    }
-                    break;
-                }
-                case "Save Permissions": {
-
-                    JFileChooser chooser = new JFileChooser();
-                    chooser.setDialogTitle("Specify a file to save");
-
-                    // Disable the all files option
-                    chooser.setAcceptAllFileFilterUsed(false);
-                    chooser.setCurrentDirectory(new java.io.File("."));
-
-                    if (chooser.showSaveDialog(dashboard) == JFileChooser.APPROVE_OPTION) {
-                        try {
-                            //TODO warn about overwriting
-                            PermissionManager.savePermissions(parameters.getPermissions(), chooser.getSelectedFile());
-                        } catch (Exception exception) {
-                            sendToConsole(exception.getMessage(), Dashboard.MessageType.ERROR);
-                        }
-                    }
-                    dashboard.togglePermissionButton();
-                    break;
-                }
-                default:
-                    throw new AssertionError(); // Defensive measure; this should never happen.
-            }
-        }
-
-    }
-
-    class PermissionListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            Permission permission = dashboard.getPermissionInput();
-            String location = parameters.getLocation();
-            parameters.setPermission(permission);
-            dashboard.setPermission(permission.toString());
-            if (house != null && location != null) {
-                try {
-                    house.addPerson("user", permission, location);
-                } catch (Exception exception) {
-                    parameters.setLocation(null);
-                    house.removePerson("user");
-                    dashboard.setLocation((String) null);
-                }
-            }
-        }
-
-    }
-
-    class EditPermissionsListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            PermissionEditor editor = dashboard.getPermissionEditor();
-            editor.addTableModelListener(f -> {
-                DefaultTableModel table = (DefaultTableModel) f.getSource();
-                for (int i = 0; i < table.getRowCount(); i++) {
-                    Action action = (Action) table.getValueAt(i, 0);
-
-                    for (int j = 1; j < table.getColumnCount(); j++) {
-                        Boolean value = (Boolean) table.getValueAt(i, j);
-
-                        if (value) { // If checkbox is selected
-                            parameters.getPermissionOf(table.getColumnName(j)).addPermission(action);
-                        } else {
-                            parameters.getPermissionOf(table.getColumnName(j)).removePermission(action);
-                        }
-                    }
-                }
-                if (dashboard.canLoadPermissions()) {
-                    dashboard.togglePermissionButton();
-                }
-            });
-
-            editor.setLocationRelativeTo(dashboard);
-            editor.pack();
-            editor.setVisible(true);
-        }
-
-    }
-
-    class LocationListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(final ActionEvent e) {
-            String location = dashboard.getLocationInput();
-            parameters.setLocation(location);
-            dashboard.setLocation(location);
-            try {
-                house.addPerson("user", parameters.getPermission(), location);
-                sendToConsole("You have entered the " + location + ".", Dashboard.MessageType.NORMAL);
-                if (parameters.isOn() && parameters.isAwayMode()) {
-                    getSecurityModuleController().startAwayModeCountdown();
-                }
-            } catch (NoSuchElementException exception) {
-                sendToConsole("You have exited the house.", Dashboard.MessageType.NORMAL);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-                sendToConsole(exception.getMessage(), Dashboard.MessageType.ERROR);
-            }
-            getCoreModuleController().toggleAutoLight();
-            redrawHouse();
-        }
-
-    }
-
-    class TemperatureListener implements ChangeListener {
-
-        @Override
-        public void stateChanged(final ChangeEvent e) {
-            int temperature = dashboard.getTemperatureInput();
-            parameters.setTemperature(temperature);
-            dashboard.setTemperature(String.valueOf(temperature));
-        }
-
-    }
-
-    class DateListener implements ChangeListener {
-
-        @Override
-        public void stateChanged(final ChangeEvent e) {
-            Date date = dashboard.getDateInput();
-            parameters.setDate(date);
-            dashboard.setDate(date);
-        }
-
-    }
-
     class ActionSelectionListener extends MouseAdapter {
 
         @Override
@@ -374,53 +120,28 @@ public class Controller {
 
     }
 
-    public void startClockDisplayUpdate() {
-        ActionListener updateClockListener = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dashboard.setTime(parameters.getClockTime());
-            }
-        };
-
-        javax.swing.Timer timerClock = new javax.swing.Timer(1000, updateClockListener);
-        timerClock.start();
-    }
-
-    class TimeXListener implements ChangeListener {
-
-        @Override
-        public void stateChanged(final ChangeEvent e) {
-            int timeX = dashboard.getTimeXInput();
-            parameters.setClockTimeMultiplier(timeX);
-        }
-
-    }
-
-    class TimeUpdateListener implements ChangeListener {
-
-        @Override
-        public void stateChanged(final ChangeEvent e) {
-            int h = dashboard.getHourInput();
-            int m = dashboard.getMinInput();
-            int s = dashboard.getSecInput();
-            int[] time = new int[] {h, m, s};
-
-            parameters.setTime(time);
-        }
-
-    }
-
-    // TODO comment this
     Parameters getParameters() {
         return parameters;
     }
 
-    // TODO comment this
     House getHouse() {
         return house;
     }
 
+    void setHouse(House house) {
+        this.house = house; // TODO null check?
+    }
+
     Dashboard getDashboard() {
         return dashboard;
+    }
+
+    CoreModuleController getCoreModuleController() {
+        return ((CoreModuleController) modules.get(Module.SHC.getName()));
+    }
+
+    SecurityModuleController getSecurityModuleController() {
+        return ((SecurityModuleController) modules.get(Module.SHP.getName()));
     }
 
     void sendToConsole(String message, Dashboard.MessageType type) {
@@ -432,14 +153,6 @@ public class Controller {
             dashboard.updateRoom(location, house.getRoom(location));
         }
         dashboard.redrawHouse();
-    }
-
-    private CoreModuleController getCoreModuleController() {
-        return ((CoreModuleController) modules.get(Module.SHC.getName()));
-    }
-
-    private SecurityModuleController getSecurityModuleController() {
-        return ((SecurityModuleController) modules.get(Module.SHP.getName()));
     }
 
 }
