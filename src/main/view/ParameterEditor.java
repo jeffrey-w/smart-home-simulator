4827e9b83b3d874c90a7afd13ff3c5342ceb6c1f
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-11-24 20:52:46 -0500

    remove inividual time field editors; add time field; remove unused constants; modify constructor

--- src/main/view/ParameterEditor.java
+++ src/main/view/ParameterEditor.java
@@ -23,9 +23,9 @@ public class ParameterEditor extends JPanel {
     private static final int BUTTON_OFFSET = 0x10;
     private static final int BUTTON_X_PADDING = 0x40;
     private static final int BUTTON_Y_PADDING = 0x20;
-    private static final int FIELD_ROWS = 8;
+    private static final int FIELD_ROWS = 6;
     private static final int FIELD_COLUMNS = 2;
-    private static final int FIELD_Y_PADDING = 0x30;
+    private static final int FIELD_Y_PADDING = 0x40;
 
     private static final Permission[] PERMISSIONS = new Permission[] {
             null,
@@ -39,20 +39,9 @@ public class ParameterEditor extends JPanel {
                     Parameters.MAX_TEMPERATURE, 1);
     private static final SpinnerDateModel DATE_MODEL =
             new SpinnerDateModel(Date.from(Instant.now()), null, null, Calendar.DAY_OF_YEAR);
-
     private static final SpinnerNumberModel TIMEX_MODEL =
             new SpinnerNumberModel(Parameters.DEFAULT_TIMEX, Parameters.MIN_TIMEX,
                     Parameters.MAX_TIMEX, 1);
-
-    private static final SpinnerNumberModel HOUR_MODEL =
-            new SpinnerNumberModel(Parameters.DEFAULT_HOURS, Parameters.MIN_HOURS,
-                    Parameters.MAX_HOURS, 1);
-    private static final SpinnerNumberModel MIN_MODEL =
-            new SpinnerNumberModel(Parameters.DEFAULT_MINS, Parameters.MIN_MINS,
-                    Parameters.MAX_MINS, 1);
-    private static final SpinnerNumberModel SEC_MODEL =
-            new SpinnerNumberModel(Parameters.DEFAULT_SECS, Parameters.MIN_SECS,
-                    Parameters.MAX_SECS, 1);
 
     static JComboBox<Permission> permissionJComboBox() {
         return new JComboBox<>(PERMISSIONS);
@@ -70,10 +59,8 @@ public class ParameterEditor extends JPanel {
     JComboBox<String> location = new JComboBox<>();
     JSpinner temperature = new JSpinner(TEMP_MODEL);
     JSpinner date = new JSpinner(DATE_MODEL);
+    JSpinner time = new JSpinner(new SpinnerDateModel());
     JSpinner timeX = new JSpinner(TIMEX_MODEL);
-    JSpinner hour = new JSpinner(HOUR_MODEL);
-    JSpinner min = new JSpinner(MIN_MODEL);
-    JSpinner sec = new JSpinner(SEC_MODEL);
 
     /**
      * Constructs a new {@code ParameterEditor} object.
@@ -110,14 +97,10 @@ public class ParameterEditor extends JPanel {
         fields.add(temperature);
         fields.add(labelFactory("Date"));
         fields.add(date);
+        fields.add(labelFactory("Time"));
+        fields.add(time);
         fields.add(labelFactory("Time Speed Multiplier"));
         fields.add(timeX);
-        fields.add(labelFactory("Time Hour"));
-        fields.add(hour);
-        fields.add(labelFactory("Time Min"));
-        fields.add(min);
-        fields.add(labelFactory("Time Sec"));
-        fields.add(sec);
 
         // Set field panel display behavior
         SpringUtilities.makeCompactGrid(fields, FIELD_ROWS, FIELD_COLUMNS, 1, 1, 1, FIELD_Y_PADDING);
@@ -125,6 +108,7 @@ public class ParameterEditor extends JPanel {
         // Selecting a location is disabled by default.
         location.setEnabled(false);
         date.setEditor(new JSpinner.DateEditor(date, "yyyy/MM/dd"));
+        time.setEditor(new JSpinner.DateEditor(time, "HH:mm:ss"));
     }
 
 }
