c2cf3c7a74d137f82be3442d1811ea0151cf5def
Philippe Vo <philippe.vo.nam@gmail.com>
2020-12-03 18:58:45 -0500

    added unitest for SeasonCheck

---
+++ src/test/main/util/SeasonCheckTest.java
@@ -0,0 +1,21 @@
+package main.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SeasonCheckTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSeason() {
+    }
+}
