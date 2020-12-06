5bfc3b693bc367623417238b6309192c6fa3c55b
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-12-06 12:44:08 -0500

    edit package statement, simplify assertions

--- src/test/main/util/SeasonCheckTest.java
+++ src/test/main/util/SeasonCheckTest.java
@@ -1,14 +1,13 @@
-package main.util;
+package test.main.util;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import main.util.SeasonCheck;
 import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class SeasonCheckTest {
 
@@ -16,15 +15,15 @@ class SeasonCheckTest {
     void testGetSeason() {
         // test the four season
         Date date = new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime();
-        assertTrue(SeasonCheck.Season.WINTER == SeasonCheck.getSeason(date));
+        assertSame(SeasonCheck.Season.WINTER, SeasonCheck.getSeason(date));
 
         date = new GregorianCalendar(2020, Calendar.APRIL, 1).getTime();
-        assertTrue(SeasonCheck.Season.SPRING == SeasonCheck.getSeason(date));
+        assertSame(SeasonCheck.Season.SPRING, SeasonCheck.getSeason(date));
 
         date = new GregorianCalendar(2020, Calendar.JULY, 1).getTime();
-        assertTrue(SeasonCheck.Season.SUMMER == SeasonCheck.getSeason(date));
+        assertSame(SeasonCheck.Season.SUMMER, SeasonCheck.getSeason(date));
 
         date = new GregorianCalendar(2020, Calendar.OCTOBER, 1).getTime();
-        assertTrue(SeasonCheck.Season.FALL == SeasonCheck.getSeason(date));
+        assertSame(SeasonCheck.Season.FALL, SeasonCheck.getSeason(date));
     }
 }
