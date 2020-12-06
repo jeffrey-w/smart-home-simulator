2ae2df380fe9de00db1b400257a1aad8bbd16cf3
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2020-12-06 02:15:14 -0500

    modify SHH

--- src/main/model/Module.java
+++ src/main/model/Module.java
@@ -41,7 +41,6 @@ public final class Module {
      * Functionality related to heating.
      */
     public static final Module SHH = (new ModuleBuilder())
-            .addCommandFor("Zones", Action.CREATE_TEMPERATURE_CONTROL_ZONE)
             .addCommandFor("Zones", Action.MANAGE_TEMPERATURE_CONTROL_ZONES)
             .addCommandFor("Temperature", Action.READ_TEMPERATURES)
             .addCommandFor("Temperature", Action.CHANGE_TEMPERATURE)
